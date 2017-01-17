5eb1d093288153c17adb14fb7a94e05d82b051bc
Martin Sivak <msivak@redhat.com>
2017-01-17 10:02:34 +0100

    Improve current date decode, cleanup deps and update readme

--- water.py
+++ water.py
@@ -2,17 +2,17 @@
 # encoding: utf-8
 
 import sys, math, binascii, struct
-from bitarray import bitarray
 from crcmod import Crc
-from collections import defaultdict
-from hamm import hamming
 from time import gmtime, strftime
-import datetime
+from datetime import datetime, timedelta
 
 # Resources:
+# http://www.rtl-sdr.com/decoding-public-utility-meters-with-an-rtl-sdr/
 # www.st.com/resource/en/application_note/dm00233038.pdf
 # http://oms-group.org/fileadmin/pdf/OMS-Spec_Vol2_Primary_v301.pdf
 # https://github.com/mjung85/iotsys/blob/master/wiki/MBusConnector.md 
+# https://forum.fhem.de/index.php/topic,42232.html
+# https://github.com/mhop/fhem-mirror/blob/master/fhem/FHEM/32_TechemHKV.pm
 
 # poly: x16+x13+x12+x11+x10+x8+x6+x5+x2+1
 poly=(1<<16)+(1<<13)+(1<<12)+(1<<11)+(1<<10)+(1<<8)+(1<<6)+(1<<5)+(1<<2)+1
@@ -28,12 +28,14 @@ CVALS = {
 }
 
 CIVALS = {
-    0x72: "RfD-12-Mb",
+    0x72: "RfD-12-Mb", # Standard wMbus response
+    0xA0: "TechemBin",
+    0xA1: "TechemBcd" # Probably
 }
 
 DEVVALS = {
-    0x62: "HotW",
-    0x80: "Heat",
+    0x62: "HotW", 
+    0x80: "Heat", # Heating meter
 }
 
 def crc(data):
@@ -41,26 +43,39 @@ def crc(data):
     crc16.update(data)
     return crc16.digest()
 
-def todate(d):
-    now = datetime.datetime.now()
+def nextdate(last_date, month, day):
+    """
+    Compute a new datetime object representing the closest
+    month and day in future (starting with the last_date)
+    """
+    next_date = last_date.replace(month = month, day = day)
+    if (next_date < last_date):
+	next_date += timedelta(years = 1)
+    return next_date
+
+def todate(last_date, d):
     d=struct.unpack('<H', d)[0]
-    return '%04d-%02d-%02d' % (
-        now.year,
-        (d >> 9) & 0x0f,
-        (d >> 4) & 0x1f)
+    return nextdate(last_date,
+        month = max(1, (d >> 9) & 0x0f),
+        day = max(1, (d >> 4) & 0x1f))
 
 def tolastdate(d):
     d=struct.unpack('<H', d)[0]
-    return '%04d-%02d-%02d' % (
-        2000 + ((d >> 9) & 0x3f),
-        (d >> 5) & 0x0f,
-        d & 0x1f)
+    return datetime(
+        year = 2000 + ((d >> 9) & 0x3f),
+        month = max(1, (d >> 5) & 0x0f), # Make sure the parsing does
+        day = max(1, d & 0x1f))          # not fail on month / day values
+
 
 def decodeid(d):
     assert len(d) == 4
     return hexdump(d[::-1])
 
 def decode2Bto3ch(vendor):
+    """
+    Decode the vendor string. Converts 2 bytes into
+    three characters.
+    """
     manuf = []
     for i in xrange(3):
         manuf.append(chr(0x40 + (vendor & 0x1f)))
@@ -71,19 +86,22 @@ def dump(decoded):
     if len(decoded) < 24:
         return
 
-    vendor = struct.unpack("<H", decoded[2:4])[0]
-    manuf = decode2Bto3ch(vendor)
+    manufraw = struct.unpack("<H", decoded[2:4])[0]
+    manuf = decode2Bto3ch(manufraw)
 
-    addr = format(ord(pkt[4]),"02x")+format(ord(pkt[5]),"02x")
     size = ord(decoded[0])
+    command = ord(decoded[1])
+    addr = decodeid(decoded[4:8])
+    version = ord(decoded[8])
+    device = ord(decoded[9])
 
     print strftime("%d %b %Y %H:%M:%S", gmtime()),
     print "<L %d>" % size,
-    print "<C %s>" %  CVALS.get(ord(decoded[1]), binascii.hexlify(decoded[1])),
+    print "<C %s>" %  CVALS.get(command, hex(command)),
     print "<M %s>" % manuf,
-    print "<addr %s>" % decodeid(decoded[4:8]),
-    print "<ver %s>" % hexdump(decoded[8]),
-    print "<dev % 4s>" % DEVVALS.get(ord(decoded[9]), hexdump(decoded[9])),
+    print "<addr %s>" % addr,
+    print "<ver %s>" % hex(version),
+    print "<dev % 4s>" % DEVVALS.get(device, hex(device)),
     print "<crc %s %s>" % (hexdump(decoded[10:12]), "ok" if crc(decoded[:10]) == decoded[10:12] else "F!"), # CRC
     decoded = decoded[12:]
     size -= 9 # CRC and len field do not count
@@ -98,26 +116,34 @@ def dump(decoded):
       block = decoded[:block_size + 2] # 16 + 2 for CRC
       decoded = decoded[block_size + 2:]
       crc_real = crc(block[:block_size])
+      ci = None
 
       print "| [L=%02d]" % block_size,
       
       if ci is None:
-          ci = CIVALS.get(ord(block[0]), hexdump(block[0]))
-          print "<CI %s>" % ci,
+          ci = ord(block[0])
           block_start = 1
 
-      # print hexdump(block[block_start:block_size]),
       print "<crc %s %s>" % (hexdump(block[block_size:]), "ok" if crc_real == block[block_size:] else "F!"), # CRC
       appdata += block[block_start:block_size]
 
+    last_date = tolastdate(appdata[1:3])
+    last_reading = struct.unpack('<H', appdata[3:5])[0]
+    status = ord(appdata[0])
+    daily_date = todate(last_date, appdata[5:7])
+    daily_reading = struct.unpack('<H', appdata[7:9])[0]
+    t1 = struct.unpack('<H', appdata[9:11])[0]/100.0
+    t2 = struct.unpack('<H', appdata[11:13])[0]/100.0
+
     print "=",
-    print "<S %s>" % bin(ord(appdata[0])),
-    print "<Last Date %s>" % tolastdate(appdata[1:3]),
-    print "<Last %d>" % struct.unpack('<H', appdata[3:5])[0],
-    print "<Date %s>" % todate(appdata[5:7]),
-    print "<Cur %d>" % struct.unpack('<H', appdata[7:9])[0],
-    print "<T1 %2.2f°C>" % (struct.unpack('<H', appdata[9:11])[0]/100.0),
-    print "<T2 %2.2f°C>" % (struct.unpack('<H', appdata[11:13])[0]/100.0),
+    print "<CI %s>" % CIVALS.get(ci, hex(ci)),
+    print "<S %s>" % bin(status),
+    print "<Last Date %s>" % last_date,
+    print "<Last %d>" % last_reading,
+    print "<Date %s>" % daily_date,
+    print "<Cur %d>" % daily_reading,
+    print "<T1 %2.2f°C>" % t1,
+    print "<T2 %2.2f°C>" % t2,
     print hexdump(appdata[13:]),
     print
 
